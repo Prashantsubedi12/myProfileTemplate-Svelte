a31049d2c8d4c8f4cb770384f62ab9447ca0a4d5
Prashantsubedi12 <sc54023@info.sangitan.ac.jp>
2026-03-18 03:15:39 +0900

    icons changed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -391,9 +391,20 @@
     .contact-card.fb:hover     { border-color: rgba(59, 130, 246, 0.6); }
 
     .contact-card .icon {
-      font-size: 2rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
       margin-bottom: 0.75rem;
     }
+
+    .contact-card .icon svg {
+      width: 2rem;
+      height: 2rem;
+    }
+
+    .contact-card.email .icon svg  { color: #a5b4fc; }
+    .contact-card.insta .icon svg  { color: #f9a8d4; }
+    .contact-card.fb .icon svg     { color: #93c5fd; }
 
     .contact-card .label {
       color: #64748b;
@@ -567,17 +578,29 @@
       </div>
       <div class="contact-grid">
         <a href="mailto:prashantsubedi718@gmail.com" class="contact-card email">
-          <div class="icon">📧</div>
+          <div class="icon">
+            <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 4H4C2.9 4 2 4.9 2 6v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/>
+            </svg>
+          </div>
           <p class="label">Email</p>
           <p class="value">prashantsubedi718@gmail.com</p>
         </a>
         <a href="https://www.instagram.com/prashantsub12/" target="_blank" rel="noopener" class="contact-card insta">
-          <div class="icon">📸</div>
+          <div class="icon">
+            <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.334 3.608 1.308.975.975 1.246 2.242 1.308 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.334 2.633-1.308 3.608-.975.975-2.242 1.246-3.608 1.308-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.334-3.608-1.308-.975-.975-1.246-2.242-1.308-3.608C2.175 15.584 2.163 15.204 2.163 12s.012-3.584.07-4.85c.062-1.366.334-2.633 1.308-3.608.975-.975 2.242-1.246 3.608-1.308C8.416 2.175 8.796 2.163 12 2.163zm0-2.163C8.741 0 8.332.013 7.052.072 5.197.157 3.355.673 2.014 2.014.673 3.355.157 5.197.072 7.052.013 8.332 0 8.741 0 12c0 3.259.013 3.668.072 4.948.085 1.855.601 3.697 1.942 5.038 1.341 1.341 3.183 1.857 5.038 1.942C8.332 23.987 8.741 24 12 24s3.668-.013 4.948-.072c1.855-.085 3.697-.601 5.038-1.942 1.341-1.341 1.857-3.183 1.942-5.038.059-1.28.072-1.689.072-4.948s-.013-3.668-.072-4.948c-.085-1.855-.601-3.697-1.942-5.038C20.645.673 18.803.157 16.948.072 15.668.013 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324A6.162 6.162 0 0 0 12 5.838zm0 10.162a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z"/>
+            </svg>
+          </div>
           <p class="label">Instagram</p>
           <p class="value">@prashantsub12</p>
         </a>
         <a href="https://www.facebook.com/kshitij.subedi.12" target="_blank" rel="noopener" class="contact-card fb">
-          <div class="icon">💬</div>
+          <div class="icon">
+            <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.025 1.792-4.697 4.532-4.697 1.312 0 2.686.236 2.686.236v2.97h-1.514c-1.491 0-1.956.93-1.956 1.886v2.267h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
+            </svg>
+          </div>
           <p class="label">Facebook</p>
           <p class="value">Prashant Subedi</p>
         </a>
